1d800e1fb104b0b3d9872adfd44568f892498168
Pedro Castro <draocastro@gmail.com>
2023-08-08 13:29:02 -0300

    atualizacao necessaria

---
+++ script.js
@@ -0,0 +1,23 @@
+const menuItems = document.querySelectorAll('.header a[href^="#"]');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick);
+})
+
+function getScrollTopByHref(element) {
+  const id = element.getAttribute('href');
+  return document.querySelector(id).offsetTop;
+}
+
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const to = getScrollTopByHref(event.target);
+  scrollToPosition(to);
+}
+
+function scrollToPosition(to) {
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  });
+}
